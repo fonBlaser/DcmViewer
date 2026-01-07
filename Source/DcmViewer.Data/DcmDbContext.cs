f87edd8f8fa885a37174de672d2e13427e7e8524
Eugene Yakush <eugene@steercrm.com>
2026-01-07 08:49:59 +0200

    Add database context with log entries

---
+++ Source/DcmViewer.Data/DcmDbContext.cs
@@ -0,0 +1,52 @@
+﻿using DcmViewer.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+
+namespace DcmViewer.Data;
+
+public class DcmDbContext : DbContext
+{
+    private readonly string _connectionString;
+
+    public DbSet<LogEntry> Logs => Set<LogEntry>();
+
+    public DcmDbContext(string filePath)
+    {
+        _connectionString = $"Data Source={filePath};Mode=ReadWriteCreate;Cache=Shared";
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite(_connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder model)
+    {
+        model.Entity<LogEntry>(e =>
+        {
+            e.ToTable("LogEntries");
+
+            e.HasKey(x => x.Id);
+
+            e.Property(x => x.Timestamp)
+                .IsRequired();
+
+            e.Property(x => x.Service)
+                .IsRequired()
+                .HasMaxLength(32);
+
+            e.Property(x => x.Module)
+                .IsRequired()
+                .HasMaxLength(32);
+
+            e.Property(x => x.Severity)
+                .IsRequired(); ;
+
+            e.HasIndex(x => x.Timestamp);
+            e.HasIndex(x => x.Service);
+            e.HasIndex(x => x.Module);
+            e.HasIndex(x => x.Severity);
+        });
+    }
+}
---
+++ Source/DcmViewer.Data/Models/LogEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace DcmViewer.Data.Models;
+
+public class LogEntry
+{
+    public long Id { get; set; }
+    public DateTime Timestamp { get; set; }
+    public Severity Severity { get; set; }
+    public string Service { get; set; }
+    public string Module { get; set; }
+    public string Message { get; set; }
+}
